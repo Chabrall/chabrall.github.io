c3e2afd292ae9b52cf4e559138db5c7a240ab741
David Andersson <david.andersson@knowit.se>
2014-10-30 00:04:36 +0100

    more cool adjustments

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		var scrollLength = $(window).scrollTop();
 		var navHeight = $('nav').height();
 		if(scrollLength >= windowHeight-navHeight)
-			$('nav').css('background', "rgba(0,0,0,.6)");
+			$('nav').css('background', "rgba(0,0,0,.8)");
 		else
 			$('nav').css('background', "none");
 	});
