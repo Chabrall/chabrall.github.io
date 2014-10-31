99b1b93a5d577b55f1ce022549451c3e9127b7a7
David Andersson <david.andersson@knowit.se>
2014-10-31 09:13:42 +0100

    removed jQuery dependency and clean up

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
-$(document).ready(function(){
-	var windowHeight = $(window).height();
+document.addEventListener('DOMContentLoaded', function(){
+	var windowHeight = window.innerHeight;
 	var iOS = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
 
-	if(iOS)
-		$('header').css('background-attachment', 'scroll');
+
+	//Fix for iOS background-attachment issues
+	if(iOS) {
+		var header = document.getElementById('header');
+		if (header.classList)
+			header.classList.add('background-scroll');
+		else
+			header.className += ' ' + 'background-scroll';
+	}
 
 	//Dimm nav background when reaching content and reverse
-	$(window).scroll(function(){
-		var scrollLength = $(window).scrollTop();
-		if(scrollLength >= windowHeight-90)
-			$('nav').css('background', "rgba(0,0,0,.8)");
-		else
-			$('nav').css('background', "none");
-	});
+	window.onscroll = function(){
+		var scrollLength = window.pageYOffset;
+		var nav = document.getElementById('nav');
+
+		if(scrollLength >= windowHeight-90) {
+			if (nav.classList)
+				nav.classList.add("nav-background");
+		}
+		else {
+	    if (nav.classList)
+	        nav.classList.remove('nav-background');
+	    else
+	        nav.className = nav.className.replace(new RegExp('(^|\\b)' + className.
+						split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+		}
+	};
 });
